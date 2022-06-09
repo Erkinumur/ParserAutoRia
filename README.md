f871778ed77c739b2cc266157cf0aa43664ec846
Erkinumur <erkin.umur@gmail.com>
2022-06-09 16:24:55 +0600

    add parse params from command line. add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+run main.py to start parser  
+  
+params:  
+- -l, --limit -  number of pages to parse
+- -p, -page - start page to parse
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import argparse
+
 from src.parsers import ParserAutoRia
 
 
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-l', '--limit', default=0, type=int)
+    arg_parser.add_argument('-p', '--page', default=1, type=int)
+    args = arg_parser.parse_args()
+
     parser = ParserAutoRia()
-    parser.start_parse()
+    parser.start_parse(limit=args.limit, start_page=args.page)
     print('finish')
+
