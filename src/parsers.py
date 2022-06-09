2d99e1fa7776da90fc7d4dba88a53fdc7ed7a8fc
Erkinumur <erkin.umur@gmail.com>
2022-06-09 17:10:17 +0600

    move all xpaths to parser class attrs

--- src/parsers.py
+++ src/parsers.py
@@ -10,8 +10,24 @@ from .models import CarInfo
 
 
 class ParserAutoRia:
+    BASE_URL = 'https://auto.ria.com/uk/car/used'
+    title_xpath = '//h1/text()'
+    sold_out_xpath = '//div[contains(@class, "sold-out")]'
+    price_usd_xpath = '//span[@data-currency="USD"]/text()'
+    price_default_xpath = '//div[@class="price_value"]/strong/text()'
+    mileage_xpath = '//div[@class="base-information bold"]/span/text()'
+    owner_name_xpath = '//h4[@class="seller_info_name bold"]/text()'
+    company_name_xpath = '//h4[@class="seller_info_name"]/a/text()'
+    auto_salon_name_xpath = '//h4[@class="seller_info_name"]/a/strong/text()'
+    phone_number_xpath = '//span[@class="phone bold"]/@data-phone-number'
+    ing_url_xpath = '//div[@class="photo-620x465"]//img/@src'
+    img_count_xpath = '//a[@class="show-all link-dotted"]/text()'
+    car_number_xpath = '//span[contains(@class, "state-num")]/text()'
+    vin_code_xpath = '//span[@class="label-vin"]/text()'
+    last_page_xpath = '//a[@class="page-link"]/text()'
+
+
     def __init__(self):
-        self.BASE_URL = 'https://auto.ria.com/uk/car/used'
         self.db = Database()
 
     def _get_response(self, url: str) -> Response:
@@ -26,7 +42,7 @@ class ParserAutoRia:
         response = self._get_response(self.BASE_URL)
         selector = Selector(response.text)
         last_page = int(
-            selector.xpath('//a[@class="page-link"]/text()')[-1]
+            selector.xpath(self.last_page_xpath)[-1]
                 .get().replace(' ', '')
         ) * 2
         print(f'lastpage - {last_page}')
@@ -43,45 +59,44 @@ class ParserAutoRia:
         response = self._get_response(url)
         selector = Selector(response.text)
 
-        if selector.xpath('//div[contains(@class, "sold-out")]'):
+        if selector.xpath(self.sold_out_xpath):
             return
 
-        price = selector.xpath('//span[@data-currency="USD"]/text()').get()
+        price = selector.xpath(self.price_usd_xpath).get()
         if not price:
-            price = selector.xpath('//div[@class="price_value"]/strong/text()').get()
+            price = selector.xpath(self.price_default_xpath).get()
         if price:
             price = int(price.replace(' ', '').replace('$', ''))
 
-        mileage = selector.xpath('//div[@class="base-information bold"]/span/text()').get()
+        mileage = selector.xpath(self.mileage_xpath).get()
         if mileage:
             mileage = int(mileage.strip())
 
-        owner_name = selector.xpath('//h4[@class="seller_info_name bold"]/text()').get()
+        owner_name = selector.xpath(self.owner_name_xpath).get()
         if not owner_name:
-            owner_name = selector.xpath('//h4[@class="seller_info_name"]/a/text()').get()
+            owner_name = selector.xpath(self.company_name_xpath).get()
         if not owner_name:
-            owner_name = selector.xpath('//h4[@class="seller_info_name"]/a/strong/text()')
+            owner_name = selector.xpath(self.auto_salon_name_xpath)
 
-        phone_number = selector.xpath(
-            '//span[@class="phone bold"]/@data-phone-number').get()
+        phone_number = selector.xpath(self.phone_number_xpath).get()
         if phone_number:
             phone_number = '+380 ' + phone_number
 
-        img_count = selector.xpath('//a[@class="show-all link-dotted"]/text()').get()
+        img_count = selector.xpath(self.img_count_xpath).get()
         if img_count:
             img_count = int([int(s) for s in img_count.split() if s.isdigit()][0])
 
-        car_number = selector.xpath('//span[contains(@class, "state-num")]/text()').get()
-        vin_code = selector.xpath('//span[@class="label-vin"]/text()').get()
+        car_number = selector.xpath(self.car_number_xpath).get()
+        vin_code = selector.xpath(self.vin_code_xpath).get()
 
         car = CarInfo(
             url=url,
-            title=selector.xpath('//h1/text()').extract_first().strip(),
+            title=selector.xpath(self.title_xpath).extract_first().strip(),
             price=price,
             mileage=mileage,
             owner_name=owner_name.strip() if owner_name else None,
             phone_number=phone_number,
-            img_url=selector.xpath('//div[@class="photo-620x465"]//img/@src').extract_first(),
+            img_url=selector.xpath(self.ing_url_xpath).extract_first(),
             img_count=img_count,
             car_number=car_number.strip() if car_number else None,
             vin_code=vin_code.strip() if vin_code else None
